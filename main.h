cf0de437e8a9bde5cf64c4727256a52da7734803
canonone <umohchristian001@gmail.com>
2023-08-22 08:26:27 -0700

    update to main.h

--- main.h
+++ main.h
@@ -4,9 +4,9 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...);
-int str_len(const char *strn);
-int print_char(char c);
-int print_string(const char *str);
+int _printf(const char *, ...);
+int str_len(const char *);
+int print_char(char);
+int print_string(const char *);
 
 #endif
